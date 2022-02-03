69ceb271cf53cc7ce90bac216747dea6d22cad75
LJSLS <ljsalas41@gmail.com>
2022-02-03 20:17:40 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ import React, {useState} from 'react';
 function App(){
   const [count,setCount]=useState(3)
   const [Rcount,RsetCount]=useState(1)
+  const Countundo = 0 
   function decCount(){
-    setCount(prevCount=>prevCount - 1)
+    if(count === 0){
+      setCount(prevCount=>prevCount + 0)
+    }else{
+      setCount(prevCount=>prevCount - 1)
+    }
   }
   function incCount(){
-    setCount(prevCount=>prevCount + 1)
+    if(count === 10){
+      setCount(prevCount=>prevCount + 0)
+    }else{
+      setCount(prevCount=>prevCount + 1)
+    }
+    
   }
   function nextTurn(){
     setCount(prevCount=>prevCount + 2)
     RsetCount(RprevCount=>RprevCount + 1)
   }
+
   function reset(){
     setCount(prevCount=>3)
     RsetCount(RprevCount=>1)
   }
+
+  function undo(){
+
+    RsetCount(RprevCount=>RprevCount - 1)
+  }
+
   return (
     <>
     <div class="demo-wrap">
@@ -27,15 +44,12 @@ function App(){
         <div className='ECOUNT'>
           <span>{count}</span>
         </div>
-        
         <br></br><br></br>
-
         <div className='EUSED'>
           <span>Energy Used</span><br></br>
         </div>
           <button className ="minusbutton" onClick={incCount}>-</button>
           <button onClick={decCount}>+</button>
-        
         
         <br></br><br></br><br></br>
         <div className='EGAIN'>
@@ -43,7 +57,7 @@ function App(){
         </div>
           <button className ="minusbutton" onClick={decCount}>-</button>
           <button onClick={incCount}>+</button>
-        
+       
         
         <br></br><br></br><br></br>
 
@@ -55,12 +69,13 @@ function App(){
         
         <br></br><br></br>
         <br></br><br></br>
-        
-        
 
         <button className="Endturn" onClick={nextTurn}>End Turn</button>
         <br></br><br></br>
-        <button className="Reset" onClick={reset}>Reset</button>
+        <div className='bottom'>
+          <button className="Reset" onClick={reset}>Reset</button>
+        </div>
+        
       </div>
     </div>
     </>
